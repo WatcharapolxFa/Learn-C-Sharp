51f66ebafe7ce31689cc0cd3b6543ed0e3a5e36a
Watcharaopol Yotadee <watcharaopol.yot@ntbg.local>
2023-12-06 13:17:35 +0700

    feat: startCode

---
+++ Basic-Programming/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            float b = 10.00f;
+            int number = 1234567; 
+            Console.WriteLine(" Hello World ! ");
+            Console.WriteLine("Number = {0:E}", number);
+            Console.WriteLine("Number = {0:G}",number);
+            Console.WriteLine("Number = {0:N}", number);
+            Console.WriteLine("Number = {0:x}", number);
+            Console.ReadKey();
+        }
+    }
+}
